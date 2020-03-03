17244b018992cd4b8e7a3ab8c6bfe2c168e79a30
bushaf <bozidar.siljanoski@live.com>
2020-03-03 10:17:10 +0100

    local dev env added to this branch

---
+++ searchpage.php
@@ -0,0 +1,10 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$content = file_get_contents('http://cardo.idxbroker.com/idx/search/basic');
+$content = str_replace('</title>','</title><base href="http://localhost/mcidx/" />', $content);
+$content = str_replace('</head>','<link rel="stylesheet" href="http://localhost/mcidx/style.css" /></head>', $content);
+$content = str_replace('lang="en" class=" "','lang="en" class="search-page"', $content);
+
+
+echo $content;
+?>
---
+++ singleproperty.php
@@ -0,0 +1,14 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$content = file_get_contents('http://cardo.idxbroker.com/idx/details/listing/d504/1430343/241-Jefferson-Lane-Cranberry-Township-PA-16066');
+$content = str_replace('</title>','</title><base href="http://localhost/mcidx/" />', $content);
+$content = str_replace('</head>','<link rel="stylesheet" href="http://localhost/mcidx/style.css" /></head>', $content);
+$content = str_replace('<html lang="en" class=" ">','<html lang="en" class="property-page">', $content);
+
+$jscript = '<script>
+    document.getElementsByTagName("html")[0].classList.add("propertypage");
+</script>';
+$content = str_replace('</html>',$jscript.'</html>', $content);
+
+echo $content;
+?>
